96bf65f4863715e3fa781af016f5b7e1d75c3b90
Dmitry <234518@mail.ru>
2021-10-01 13:43:15 +0300

    getRecords

--- public/script.js
+++ public/script.js
@@ -20,4 +20,23 @@ document.querySelectorAll(".switch").forEach(el=>
             el.parentNode.classList.add("on");
         }
     })
-})
+});
+
+getRecords();
+
+
+function getRecords()
+{
+    fetch("/api/records").then(r=>r.json()).then(obj=>{
+        let entries = Object.entries(obj);
+        let list = document.querySelector("#records");
+        list.innerHTML = "";
+        for(let entry of entries)
+        {
+            let li = document.createElement("li");
+            li.innerText = `${entry[0]} ${entry[1]}`;
+            list.append(li);
+        }
+
+    });
+}
